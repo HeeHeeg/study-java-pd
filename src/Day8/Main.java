276c4ab0cc15de55adf3ccb066904eaadf0012bd
HeeHeeg <qmffndoden@naver.com>
2023-06-01 14:08:16 +0900

    D8

--- src/Day8/Main.java
+++ src/Day8/Main.java
@@ -1,7 +1,14 @@
 package Day8;
 
+import Day8.controller.Controller;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //싱글톤
+        Scanner sc = new Scanner(System.in);
+        Controller controller = new Controller(sc);
+        while (true) controller.selectMode();
     }
 }
