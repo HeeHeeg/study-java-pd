08087cf4b3e791648ca33cd93d5e52291736b7d7
HeeHeeg <qmffndoden@naver.com>
2023-06-08 13:36:47 +0900

    jdbc

---
+++ src/jdbc_/src/config/JdbcConnection.java
@@ -0,0 +1,26 @@
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JdbcConnection {
+
+    private final String url = "jdbc:mysql://localhost:3306/test2"+
+            "?serverTimezone=Asia/Seoul&characterEncoding=UTF-8";
+
+    private final String root = "root";
+    private final String password = "[dhqclwoa12]";
+
+
+    public Connection  getJdbc() {
+        Connection conn;
+        try {
+            conn = DriverManager.getConnection(url, root, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+//        System.out.println("연동 성공");
+        return conn;
+    }
+}
---
+++ src/jdbc_/src/domain/entity/Me.java
@@ -0,0 +1,31 @@
+package domain.entity;
+
+public class Me {
+    private int id;
+
+    private String name;
+
+    public Me(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public Me() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Me{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
